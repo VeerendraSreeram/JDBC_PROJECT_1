c252cbd64ec4d4f265c1279135c2e158f09c7864
VEERENDRA SREERAM <sreeramveerendra@gmail.com>
2025-05-06 09:33:06 +0530

    Update Program_01.java

--- src/com/test/Program_01.java
+++ src/com/test/Program_01.java
@@ -1,9 +1,14 @@
 package com.test;
-
+import java.util.Scanner;
 public class Program_01 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the n value: ");
+		int n = sc.nextInt();
+		System.out.print("Square"+n*n);
+			
 	}
 
 }
